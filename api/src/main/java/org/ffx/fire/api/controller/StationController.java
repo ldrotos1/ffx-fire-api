f90615f1b60091d3db65d2b7cf690e6e2dd6a47b
Louis <louis.drotos@gmail.com>
2024-06-23 21:26:54 -0400

    Added logging to station service

--- api/src/main/java/org/ffx/fire/api/controller/StationController.java
+++ api/src/main/java/org/ffx/fire/api/controller/StationController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.ffx.fire.models.station.StationListItem;
 import org.ffx.fire.station_service.interfaces.StationServiceInterface;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +19,14 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name="Stations API")
 public class StationController {
 
+	private static final Logger logger = LoggerFactory.getLogger(StationController.class);
+	
 	@Autowired
 	private StationServiceInterface stationServiceInterface;
 	
 	@GetMapping("/list")
 	public List<StationListItem> getStations() {
+		logger.info("API request to get all stations");
 		return stationServiceInterface.getStationList();
 	}
 }
--- services/station-service/src/main/java/org/ffx/fire/station_service/services/StationService.java
+++ services/station-service/src/main/java/org/ffx/fire/station_service/services/StationService.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 
 import org.ffx.fire.models.station.StationListItem;
 import org.ffx.fire.station_service.repository.StationListItemRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.ffx.fire.station_service.interfaces.StationServiceInterface;
 import org.ffx.fire.station_service.mappers.StationMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +15,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class StationService implements StationServiceInterface {
 	
+	private static final Logger logger = LoggerFactory.getLogger(StationService.class);
+	
 	@Autowired
 	private StationListItemRepository stationListItemRepo;
 	
 	public List<StationListItem> getStationList() {
+		logger.info("Call to station service to get all stations");
 		return stationListItemRepo.findAll().stream()
 				.map(e -> StationMapper.mapStationListItem(e))
 				.collect(Collectors.toList());
